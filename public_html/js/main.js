b9e981b84294ab9ce2b08a2448d4baa51a970863
esteban gomez <exteban.1225@gmail.com>
2015-03-12 22:46:16 -0500

    Aprendiendo cosas basicas de css y Scripts

---
+++ public_html/js/main.js
@@ -0,0 +1,8 @@
+function nombre(nombreProfesor){
+    return "Soy el profesor "+nombreProfesor;
+}
+
+function cerrarSesion(){
+    var respuesta=confirm("¿Esta seguro que desea cerrar sesión?");
+    return respuesta;
+}
